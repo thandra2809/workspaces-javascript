b2b27a1f0dad4ee75d7f63eee58a23cf97acab64
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-13 00:46:42 -0800

    added new code

--- 1-js-arrays-steven-hancock/2-section/7-chapter/app.js
+++ 1-js-arrays-steven-hancock/2-section/7-chapter/app.js
@@ -1,3 +1,13 @@
 let array = [1, 2, 3, 4, true, "array", {one: 1}, [3, 5]];
 
 console.log(array[9]);
+
+console.log(!array[9]);
+
+array[9]=NaN;
+
+if (!array[9]) {
+    console.log('This is Null or undefined');
+} else {
+    console.log('This has a value don\'t worry');
+}
