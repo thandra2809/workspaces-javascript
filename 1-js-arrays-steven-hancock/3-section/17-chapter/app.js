d289d754c936dd3e6781a1b92a336652971d425c
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-16 01:23:47 -0800

    added some code

---
+++ 1-js-arrays-steven-hancock/3-section/17-chapter/app.js
@@ -0,0 +1,9 @@
+let array =[3,15,25,6,8,12];
+
+for(let key in array){
+    console.log('*******************');
+    console.log('Key :: ',key);
+    console.log('*******************');
+    console.log('Value :: ',array[key]);
+    console.log('===================');
+}
---
+++ 1-js-arrays-steven-hancock/3-section/18-chapter/app.js
@@ -0,0 +1,5 @@
+let array = [3, 15, 25, 6, 8, 12];
+
+for (let item of array) {
+    console.log(item);
+}
