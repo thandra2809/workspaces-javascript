5845885d07b7a334d3704081cbf563096e8913af
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-16 01:08:53 -0800

    added some code

---
+++ 1-js-arrays-steven-hancock/3-section/15-chapter/app.js
@@ -0,0 +1,9 @@
+let array = [3, 15, 25, 6, 8, 12];
+let sampleArray = [];
+for (let i = 0; i < array.length; i++) {
+    sampleArray.push(array[i] + 1);
+}
+
+console.log(array);
+
+console.log(sampleArray);
