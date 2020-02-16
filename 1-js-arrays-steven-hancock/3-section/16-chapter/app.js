eb0d18ed00b18785f019222c126a7fda5fcc89b7
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-16 01:17:19 -0800

    added some code

--- 1-js-arrays-steven-hancock/3-section/16-chapter/app.js
+++ 1-js-arrays-steven-hancock/3-section/16-chapter/app.js
@@ -1,5 +1,5 @@
 let students = ["Steven", "Lori", "james", "lynette", "Ariana", "McKay", "witney"];
-let lStudents = []
+let lStudents = [];
 
 for (let i = 0; i < students.length; i++) {
     lStudents.push(students[i].toLowerCase());
