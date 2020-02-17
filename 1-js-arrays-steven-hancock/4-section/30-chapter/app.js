98ee254f746e04d1f81b395c9c13d3c532bceaf0
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-16 19:01:53 -0800

    added some code

---
+++ 1-js-arrays-steven-hancock/4-section/30-chapter/app.js
@@ -0,0 +1,11 @@
+let scores = [90, 80, 75, 95, 60, 50, 0, 40, 100, 70];
+
+let idx = 5;
+
+console.log(scores[idx]);
+
+console.log(scores[idx - 2]);
+
+console.log(scores[idx + 15]);
+
+console.log(eval((scores[idx + 15])));
