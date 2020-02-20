cec1d104cc8cbfb79811dd05b50fac12be2f05ca
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-02-20 00:09:39 -0800

    Added changes

---
+++ 1-learn-modern-js-steven-hancock/4-section/14-chapter/var-app.js
@@ -0,0 +1,19 @@
+var varFunction = function () {
+    console.log("The value of varX is " + varX);
+};
+
+varFunction();
+
+var varX = 10;
+
+console.log('*******************************');
+
+var vasrSample2 = function () {
+
+    for (var i = 0; i < 5; i++) {
+        console.log('value of i is', i);
+    }
+    console.log(i);
+};
+
+vasrSample2();
---
+++ 1-learn-modern-js-steven-hancock/4-section/15-chapter/let-app.js
@@ -0,0 +1,19 @@
+/**var letFunction = function () {
+    console.log("The value of letX is " + letX);
+};
+
+letFunction();
+
+let letX = 10;**/
+
+console.log('*******************************');
+
+var letSample2 = function () {
+
+    for (let i = 0; i < 5; i++) {
+        console.log('value of i is', i);
+    }
+    console.log(i);
+};
+
+letSample2();
