0133443b90ccafb13391f18d40f4f8895fa9ee61
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-11-18 22:12:10 -0800

    Feature Development
    Added changes

--- 1-learn-modern-js-steven-hancock/6-section-arrays/52-chapter/sparce-array.js
+++ 1-learn-modern-js-steven-hancock/6-section-arrays/52-chapter/sparce-array.js
@@ -3,5 +3,7 @@ var grades = [10, 20, 30, , 50, 60];
 for (var i = 0; i < grades.length; i++) {
     console.log(grades[i]);
 }
-
+// Testing
 // write the same logic using filter and for-each
+
+// Testing-2
